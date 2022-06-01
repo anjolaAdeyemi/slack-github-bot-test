b2ce0ca431b6166e841fa3a447e4e5620e7a4f4b
anjolaAdeyemi <104791792+anjolaAdeyemi@users.noreply.github.com>
2022-05-31 21:53:36 -0400

    develops first commit

--- github_repo.py
+++ github_repo.py
@@ -5,3 +5,6 @@ url = f"https://api.github.com/users/defunkt"
 user_data = requests.get(url).json();
 pprint(user_data);
 # https://api.github.com/repos/:owner/:repo/pulls?state=all
+
+# comments for develop
+# comments for develop
